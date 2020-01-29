840f36969e88799ac59d880902768db565f369d9
Andreas Müller <mail@devmount.de>
2020-01-29 12:58:09 +0100

    ➕ add `array_unique`

---
+++ test_array_unique.php
@@ -0,0 +1,33 @@
+<?php
+// Require vender autoload
+require_once('vendor/autoload.php');
+
+// Add namespace at the top
+use Performance\Performance;
+
+// "array_unique"
+$max = 5000000;
+$a1 = range(1,$max,3);
+$a2 = range(1,$max,2);
+$a = array_merge($a1,$a2);
+
+// obvious method
+Performance::point('array_unique');
+$result = array_unique($a);
+Performance::finish();
+
+// alternative method
+Performance::point('array_keys:array_flip');
+$result = array_keys(array_flip($a));
+Performance::finish();
+
+// Finish all tasks and show test results
+Performance::results();
+
+$export = Performance::export();
+$points = json_decode($export->toJson())->points;
+$p2 = end($points);
+$p1 = prev($points);
+
+print_r('Alternative method is ' . round(1-$p2->differenceTime/$p1->differenceTime,4)*100 . "% faster\n");
+print_r('Array with ' . count($a) . ' elements having ' . count($result) . " unique elements\n");
